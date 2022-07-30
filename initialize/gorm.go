c3d1ef82f5bc9bef0d7251f2bf75839187aa921a
谭晓珊 <tanxiaoshan@weimiao.cn>
2022-07-30 11:10:06 +0800

    update

--- initialize/gorm.go
+++ initialize/gorm.go
@@ -21,8 +21,14 @@ func initGorm() {
 	}
 	// user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%t&loc=%s",
-		mysqlConfig.User, mysqlConfig.Password, mysqlConfig.Host, mysqlConfig.Port, mysqlConfig.Database, mysqlConfig.Charset,
-		mysqlConfig.ParseTime, mysqlConfig.TimeZone)
+		mysqlConfig.User,
+		mysqlConfig.Password,
+		mysqlConfig.Host,
+		mysqlConfig.Port,
+		mysqlConfig.Database,
+		mysqlConfig.Charset,
+		mysqlConfig.ParseTime,
+		mysqlConfig.TimeZone)
 	// 设置gorm配置
 	gormConfig := &gorm.Config{
 		SkipDefaultTransaction: mysqlConfig.Gorm.SkipDefaultTx, //是否跳过默认事务
